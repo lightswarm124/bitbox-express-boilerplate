a1afee64d39388397ad679d57031f6b3fe195b3a
lightswarm124 <lightswarm@gmail.com>
2019-01-02 14:13:58 -0500

    minor express server changes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan  from 'morgan';
 import helmet  from 'helmet';
 import axios from 'axios';
 import bodyParser  from 'body-parser';
+import cors from 'cors';
 
 import middlewares  from './middlewares/middlewares';
 import wrap  from './middlewares/wrap';
@@ -13,10 +14,11 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(cors());
 
 app.use('/', routes);
 
